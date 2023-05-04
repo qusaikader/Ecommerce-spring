3f8a3860942eea03c2345d0b07517449ecac4e06
Qusai Kader <qusai.kader@somaiya.edu>
2023-05-04 19:02:07 +0530

    Setup admin page in spring

---
+++ src/main/java/com/sheryians/major/controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.sheryians.major.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+    @GetMapping("/admin")
+    public String adminHome() {
+        return "adminHome";
+    }
+}
