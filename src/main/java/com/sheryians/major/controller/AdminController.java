e1afd54ce0487d4ee387388d093b949b674b3eb8
Bharat <mbharat04@gmail.com>
2023-05-05 12:28:44 +0530

    Additional product add method creation

--- src/main/java/com/sheryians/major/controller/AdminController.java
+++ src/main/java/com/sheryians/major/controller/AdminController.java
@@ -5,6 +5,10 @@ import com.sheryians.major.model.*;
 import com.sheryians.major.service.CategoryService;
 import com.sheryians.major.service.ProductService;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +16,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -73,4 +79,29 @@ public class AdminController {
        model.addAttribute("categories",categoryService.getAllCategory());
         return "productsAdd";
     }
+    @PostMapping("/admin/products/add")
+    public String productAddPost(@ModelAttribute("productDTO")ProductDTO productDTO,
+    @RequestParam("productImage")MultipartFile file,
+    @RequestParam("imgName")String imgName) throws IOException{
+      Product product=new Product();
+      product.setId(productDTO.getId());
+      product.setName(productDTO.getName());
+      product.setCategory(categoryService.getCategoryById(productDTO.getCategoryId()).get());  
+      product.setPrice(productDTO.getPrice());
+      product.setWeight(productDTO.getWeight());
+      product.setDescription(productDTO.getDescription());
+      String imageUUID;
+      if(!file.isEmpty()){
+        imageUUID=file.getOriginalFilename();
+        Path fileNameAndPath=Paths.get(uploadDir,imageUUID);
+        Files.write(fileNameAndPath, file.getBytes());
+      }else{
+        imageUUID=imgName;
+      }  
+      product.setImageName(imageUUID);
+      productService.addProduct(product);
+        return "redirect:/admin/products";
+    }
+
+
 }
